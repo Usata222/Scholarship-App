8591ab9695c3d7249ecc32b2ab81a52d55fdd83f
Usata <tekkiusata222@gmail.com>
2026-09-09 03:44:34 +0100

    email

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -136,6 +136,11 @@ LOGIN_URL = 'admin_login'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'noreply@scholarhub.local'
-ADMIN_EMAIL = 'tekkiusata222@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+ADMIN_EMAIL = 'usatazira@gmail.com'
